126aeb45d88a7e56e33f0e204424619751f45b72
Manil munjal <111218259+manilmunjal@users.noreply.github.com>
2024-11-13 12:40:57 +0530

    Update provider.tf

--- terraform-vpc/provider.tf
+++ terraform-vpc/provider.tf
@@ -5,10 +5,14 @@ terraform {
       version = "5.44.0"
     }
   }
+
   backend "s3" {
-    bucket = "keepthisnameuniqueworldwideyoucanusenumberlike8976"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "keepthisnameuniqueworldwideyoucanusenumberlike8976"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+
   }
 }
 
